2e13e282a4b17cca695dea6b96c6303fdbc0827d
harrand <harrysjh98@gmail.com>
2024-04-19 17:18:09 +0100

    [cpp] started work on a proper type system

---
+++ cpp/src/type.cpp
@@ -0,0 +1,75 @@
+#include "type.hpp"
+
+bool type::is_undefined() const
+{
+	return !std::holds_alternative<std::monostate>(this->ty);
+}
+
+bool type::is_primitive() const
+{
+	return std::holds_alternative<primitive_type>(this->ty)
+		&& !this->is_pointer();
+}
+
+bool type::is_void() const
+{
+	return this->is_primitive()
+		&& std::get<primitive_type>(this->ty) == primitive_type::u0
+		&& !this->is_pointer();
+}
+
+bool type::is_pointer() const
+{
+	return !this->is_undefined() && this->pointer_level > 0;
+}
+
+bool type::is_struct() const
+{
+	return std::holds_alternative<struct_type>(this->ty) && !this->is_pointer();
+}
+
+std::string type::name() const
+{
+	if(this->is_undefined())
+	{
+		return "undefined";
+	}
+	std::string ret;
+
+	if(this->is_primitive())
+	{
+		ret = primitive_type_names[static_cast<int>(std::get<primitive_type>(this->ty))];
+	}
+	else
+	{
+		ret = std::get<struct_type>(this->ty).name;
+	}
+
+	for(std::size_t i = 0; i < this->pointer_level; i++)
+	{
+		ret += "*";
+	}	
+	return ret;
+}
+
+type type::dereference() const
+{
+	if(this->is_pointer())
+	{
+		type ret = *this;
+		ret.pointer_level--;
+		return ret;
+	}
+	return {};
+}
+
+type type::reference() const
+{
+	if(this->is_undefined())
+	{
+		return {};
+	}
+	type ret = *this;
+	ret.pointer_level++;
+	return ret;
+}
---
+++ cpp/src/type.hpp
@@ -0,0 +1,82 @@
+#ifndef PSYC_TYPE_HPP
+#define PSYC_TYPE_HPP
+#include <array>
+#include <variant>
+#include <string>
+
+enum class primitive_type
+{
+	i64,
+	i32,
+	i16,
+	i8,
+
+	u64,
+	u32,
+	u16,
+	u8,
+	u0,
+
+	boolean,
+
+	f64,
+	f32,
+	f16,
+	_count
+};
+
+constexpr std::array<const char*, static_cast<int>(primitive_type::_count)> primitive_type_names
+{
+	"i64",
+	"i32",
+	"i16",
+	"i8",
+
+	"u64",
+	"u32",
+	"u16",
+	"u8",
+	"u0",
+
+	"bool",
+
+	"f64",
+	"f32",
+	"f16"
+};
+
+struct struct_type
+{
+	std::string name;
+};
+
+constexpr std::size_t array_size_dyn_array = std::numeric_limits<std::size_t>::max();
+
+struct type
+{
+	std::variant
+	<
+		primitive_type,
+		struct_type,
+		std::monostate
+	> ty = std::monostate{};
+	std::size_t pointer_level = 0;
+
+	bool operator==(const type& rhs) const = default;
+
+	bool is_undefined() const; // an ill-defined type.
+
+	bool is_primitive() const;
+	bool is_void() const;
+	bool is_pointer() const;
+	bool is_struct() const;
+
+	std::string name() const;
+
+	type dereference() const;
+	type reference() const;
+
+	static type undefined();
+};
+
+#endif // PSYC_TYPE_HPP
