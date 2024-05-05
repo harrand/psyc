0e79402d10945bd830c649e1ab341275016c4c2c
harrand <harrysjh98@gmail.com>
2024-05-05 03:07:59 +0100

    [cpp] skeleton code for an ast node

--- cpp/src/ast.hpp
+++ cpp/src/ast.hpp
@@ -1,9 +1,16 @@
 #ifndef PSYC_AST_HPP
 #define PSYC_AST_HPP
+#include "srcloc.hpp"
+#include <variant>
 
 struct ast
 {
-
+	struct node
+	{
+		using payload_t = std::variant<int>;
+		payload_t payload;
+		srcloc meta;
+	};
 };
 
 #endif // PSYC_AST_HPP
