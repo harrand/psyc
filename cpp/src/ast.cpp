4c6af9383f42f3d59301b0b41214719b0fe18942
harrand <harrysjh98@gmail.com>
2024-05-05 17:28:09 +0100

    [cpp] added ast::try_get_previous and try_get_next. i have a feeling they might be way more useful within a bottom-up parser (which is what i intend to try)

--- cpp/src/ast.cpp
+++ cpp/src/ast.cpp
@@ -1,2 +1,59 @@
 #include "ast.hpp"
+#include "diag.hpp"
 
+const ast::node& ast::get(ast::path_const_view_t path) const
+{
+	const ast::node* nodeptr = &this->root;
+	for(std::size_t i : path)
+	{
+		diag::assert_that(nodeptr != nullptr, error_code::ice, "attempt to retrieve child {} of a null node", i);
+		diag::assert_that(nodeptr->children.size() > i, error_code::ice, "attempt to retrieve child {} of a node with only {} children", i, nodeptr->children.size());
+		nodeptr = &nodeptr->children[i];
+	}
+	return *nodeptr;
+}
+
+ast::node& ast::get(ast::path_const_view_t path)
+{
+	return const_cast<ast::node&>(const_cast<const ast*>(this)->get(path));
+}
+
+std::optional<ast::path_t> ast::try_get_previous(ast::path_t path) const
+{
+	std::size_t back = path.back();
+	if(back == 0)
+	{
+		return std::nullopt;
+	}
+	path.pop_back();
+	const node& parent = this->get(path);
+	if(parent.children.size() > (back - 1))
+	{
+		path.push_back(back - 1);
+		return path;
+	}
+	return std::nullopt;
+}
+
+std::optional<ast::path_t> ast::try_get_next(ast::path_t path) const
+{
+	std::size_t back = path.back();
+	path.pop_back();
+	const node& parent = this->get(path);
+	if(parent.children.size() > (back + 1))
+	{
+		path.push_back(back + 1);
+		return path;
+	}
+	return std::nullopt;
+}
+
+const ast::node& ast::current() const
+{
+	return this->get(this->current_path);
+}
+
+ast::node& ast::current()
+{
+	return this->get(this->current_path);
+}
--- cpp/src/ast.hpp
+++ cpp/src/ast.hpp
@@ -2,6 +2,7 @@
 #define PSYC_AST_HPP
 #include "srcloc.hpp"
 #include <variant>
+#include <span>
 
 struct ast
 {
@@ -21,23 +22,57 @@ struct ast
 			return std::format("decimal-literal({})", val);
 		}
 	};
+	struct identifier
+	{
+		std::string iden;
+		constexpr std::string to_string() const
+		{
+			return std::format("identifier({})", iden);
+		}
+	};
 
 	struct node
 	{
-		using payload_t = std::variant<integer_literal, decimal_literal>;
+		using payload_t = std::variant<std::monostate, integer_literal, decimal_literal, identifier>;
 		payload_t payload;
 		srcloc meta;
+		std::vector<node> children = {};
 
 		constexpr std::string to_string() const
 		{
 			std::string ret;
 			std::visit([&ret](auto&& arg)
 			{
-				ret = arg.to_string();
+				if constexpr(std::is_same_v<std::decay_t<decltype(arg)>, std::monostate>)
+				{
+					ret = "<empty>";
+				}
+				else
+				{
+					ret = arg.to_string();
+				}
 			}, this->payload);
 			return ret;
 		}
 	};
+
+	using path_t = std::vector<std::size_t>;
+	using path_const_view_t = std::span<const std::size_t>;
+	using path_view_t = std::span<std::size_t>;
+	
+	const node& get(path_const_view_t path) const;
+	node& get(path_const_view_t path);
+
+	// given a path, retrieve a path pointing to the previous node (i.e the node before this path with the same parent). if it can't be done, return nullopt.
+	std::optional<path_t> try_get_previous(path_t path) const;
+	// given a path, retrieve a path pointing to the next node (i.e the node after this path with the same parent). if it can't be done, return nullopt.
+	std::optional<path_t> try_get_next(path_t path) const;
+
+	const node& current() const;
+	node& current();
+
+	node root;
+	path_t current_path = {};
 };
 
 #endif // PSYC_AST_HPP
