e4e4a384f7d0838c48bf58d1581890b4b7b0cfb1
Harrand <harrysjh98@gmail.com>
2024-07-04 03:02:00 +0100

    [parse] function-decl will now slurp a proceeding block as a child of itself. functions with impleemntations suddenly respect the tree structure of an AST

--- cpp/src2/parse_function_decl.cpp
+++ cpp/src2/parse_function_decl.cpp
@@ -26,6 +26,21 @@ CHORD_BEGIN
 	return {.t = result::type::error, .errmsg = std::format("assignment rhs of a function declaration {} must be one of the following: `extern`. the value \"{}\" is unrecognised", fn.func_name.iden, value.iden)};
 CHORD_END
 
+// function-decl block
+// set the blk as the function-decl's only child. error out if the decl already has one or more child.
+CHORD_BEGIN
+	STATE(NODE(function_decl), NODE(block))
+	syntax::node::function_decl fn = GETNODE(function_decl);
+	syntax::node::block blk = GETNODE(block);
+	if(fn.children.size())
+	{
+		return {.t = result::type::error, .errmsg = std::format("detected multiple implementation blocks for function \"{}\"", fn.func_name.iden)};
+	}
+	fn.children.push_back(blk.unique_clone());
+	REDUCE_TO(function_decl, fn);
+	return {.t = result::type::reduce_success};
+CHORD_END
+
 #ifndef INFUNC
 }}
 #endif
