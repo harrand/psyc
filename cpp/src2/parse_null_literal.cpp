9b0137ac2f40c7e83bdd22c986d6a63fe2fe6a16
Harrand <harrysjh98@gmail.com>
2024-07-06 04:50:39 +0100

    [parse] null literals now work with casts and the new binary operators

--- cpp/src2/parse_null_literal.cpp
+++ cpp/src2/parse_null_literal.cpp
@@ -33,6 +33,51 @@ CHORD_BEGIN
 	return {.t = result::type::reduce_success};
 CHORD_END
 
+// nulllit@
+// turn into expr but keep the cast
+CHORD_BEGIN
+	STATE(NODE(null_literal), TOKEN(cast))
+	syntax::node::null_literal nulllit = GETNODE(null_literal);
+	REDUCE_TO_ADVANCED(0, 1, expression, syntax::node::expression::type::null_literal, nulllit.unique_clone());
+	return {.t = result::type::reduce_success};
+CHORD_END
+
+// nulllit+
+// turn into expr but keep the plus
+CHORD_BEGIN
+	STATE(NODE(null_literal), TOKEN(plus))
+	syntax::node::null_literal nulllit = GETNODE(null_literal);
+	REDUCE_TO_ADVANCED(0, 1, expression, syntax::node::expression::type::null_literal, nulllit.unique_clone());
+	return {.t = result::type::reduce_success};
+CHORD_END
+
+// nulllit-
+// turn into expr but keep the minus
+CHORD_BEGIN
+	STATE(NODE(null_literal), TOKEN(minus))
+	syntax::node::null_literal nulllit = GETNODE(null_literal);
+	REDUCE_TO_ADVANCED(0, 1, expression, syntax::node::expression::type::null_literal, nulllit.unique_clone());
+	return {.t = result::type::reduce_success};
+CHORD_END
+
+// nulllit*
+// turn into expr but keep the multiply
+CHORD_BEGIN
+	STATE(NODE(null_literal), TOKEN(asterisk))
+	syntax::node::null_literal nulllit = GETNODE(null_literal);
+	REDUCE_TO_ADVANCED(0, 1, expression, syntax::node::expression::type::null_literal, nulllit.unique_clone());
+	return {.t = result::type::reduce_success};
+CHORD_END
+
+// nulllit/
+// turn into expr but keep the divide
+CHORD_BEGIN
+	STATE(NODE(null_literal), TOKEN(slash))
+	syntax::node::null_literal nulllit = GETNODE(null_literal);
+	REDUCE_TO_ADVANCED(0, 1, expression, syntax::node::expression::type::null_literal, nulllit.unique_clone());
+	return {.t = result::type::reduce_success};
+CHORD_END
+
 #ifndef INFUNC
 }}
 #endif
