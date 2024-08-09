ce5e0899fdb89a1cf33927be47f0dcb97ad1bfb8
Harrand <harrysjh98@gmail.com>
2024-08-09 04:03:26 +0100

    [cpp] integrate mimalloc

--- cpp/src2/profile.cpp
+++ cpp/src2/profile.cpp
@@ -1,16 +1,19 @@
 #include "profile.hpp"
 #ifdef PSYC_PROFILE
 #include <cstddef>
+#include "mimalloc.h"
 void * operator new ( std :: size_t count )
 {
-	auto ptr = malloc ( count );
+	auto ptr = mi_malloc( count );
 	TracyAlloc ( ptr , count );
 	return ptr ;
 }
 void operator delete ( void * ptr ) noexcept
 {
 	TracyFree ( ptr );
-	free ( ptr );
+	mi_free ( ptr );
 }
 
+#else
+#include "mimalloc-override.h"
 #endif
