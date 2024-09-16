f436554b1235cd4a2767e4034c5b8399e3105af3
Harrand <harrysjh98@gmail.com>
2024-09-16 16:51:24 +0100

    [semal] place struct/function in local file unit if its not specified to be a part of a module via an attribute

--- cpp/src2/semal.cpp
+++ cpp/src2/semal.cpp
@@ -16,9 +16,9 @@ namespace semal
 		}
 	}
 
-	std::unordered_map<std::size_t, void(*)(program&, syntax::node::path_view_t, const syntax::node&)> semal_table;
+	std::unordered_map<std::size_t, void(*)(program&, syntax::node::path_view_t, const syntax::node&, const std::string&)> semal_table;
 
-	void analyse_node(program& u, syntax::node::path_view_t path, const syntax::node& n)
+	void analyse_node(program& u, syntax::node::path_view_t path, const syntax::node& n, const std::string& filename)
 	{
 		PROFZONE("semal node");
 		const char* name = n.name();
@@ -28,7 +28,7 @@ namespace semal
 		auto iter = semal_table.find(hash);
 		if(iter != semal_table.end())
 		{
-			iter->second(u, path, n);
+			iter->second(u, path, n, filename);
 		}
 		PROFZONE_END(visit);
 	}
@@ -37,11 +37,12 @@ namespace semal
 	{
 		PROFZONE("semal file");
 		diag::assert_that(std::holds_alternative<syntax::root>(ast.payload), error_code::ice, "root-node of file AST is not a root node, it is instead a {}", ast.name());
+		std::filesystem::path filename = std::get<syntax::root>(ast.payload).source_file;
 		program ret;
-		PROFNAMEF("file: %s", std::get<syntax::root>(ast.payload).source_file.string().c_str());
-		ast.iterate([&ret](syntax::node::path_view_t path, const syntax::node& node)
+		PROFNAMEF("file: %s", filename.string().c_str());
+		ast.iterate([&ret, &filename](syntax::node::path_view_t path, const syntax::node& node)
 		{
-			analyse_node(ret, path, node);
+			analyse_node(ret, path, node, filename.string());
 		});
 		return ret;
 	}
@@ -89,32 +90,41 @@ namespace semal
 		return prog.module_units[module_name];
 	}
 
+	unit& ensure_local_file(program& prog, const std::string& file)
+	{
+		prog.local_file_units[file].name = file;
+		return prog.local_file_units[file];
+	}
+
+	template<typename N>
+	unit& get_unit(program& prog, const N& node, const std::string& file)
+	{
+		std::string module_name = get_module_name_of(node);
+		if(module_name.empty())
+		{
+			return ensure_local_file(prog, file);
+		}
+		return ensure_module(prog, module_name);
+	}
+
 	void populate_semal_table()
 	{
 		PROFZONE("semal table populate");
-		#define SEMAL_CHORD(ty) semal_table[syntax::node{.payload = syntax::ty{}}.hash()] = [](program& prog, syntax::node::path_view_t path, const syntax::node& node_){auto node = NODE_AS(node_, ty);
+		#define SEMAL_CHORD(ty) semal_table[syntax::node{.payload = syntax::ty{}}.hash()] = [](program& prog, syntax::node::path_view_t path, const syntax::node& node_, const std::string& file){auto node = NODE_AS(node_, ty);
 		#define SEMAL_END };
 		SEMAL_CHORD(capped_struct_decl)
-			std::string module_name = get_module_name_of(node);
-			if(module_name.size())
-			{
-				ensure_module(prog, module_name).structs.push_back
-				({
-					.node = node,
-					.vis = visibility::global
-				});
-			}
+			get_unit(prog, node, file).structs.push_back
+			({
+				.node = node,
+				.vis = visibility::global
+			});
 		SEMAL_END
 		SEMAL_CHORD(capped_function_decl)
-			std::string module_name = get_module_name_of(node);
-			if(module_name.size())
-			{
-				ensure_module(prog, module_name).functions.push_back
-				({
-					.node = node,
-					.vis = visibility::global
-				});
-			}
+			get_unit(prog, node, file).functions.push_back
+			({
+				.node = node,
+				.vis = visibility::global
+			});
 		SEMAL_END
 	}
 }
