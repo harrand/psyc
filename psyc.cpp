25d179fac912d338bc75249016f08d2f1f311d0d
harrand <harrandev@gmail.com>
2024-12-12 18:54:44 +0000

    [cpp] added panic function

--- psyc.cpp
+++ psyc.cpp
@@ -1,6 +1,21 @@
-#include <stdio.h>
+#include <print>
+#include <cstdlib>
+#include <source_location>
+#include <stacktrace>
 
-int main()
+struct compile_args
 {
-	printf("hello world");
+
+};
+
+void panic(const char* msg, std::source_location loc = std::source_location::current(), std::stacktrace trace = std::stacktrace::current())
+{
+	std::print("\033[1;31minternal compiler error: {}\n\tat {}({}:{}) `{}`\nstacktrace:\n==========\n{}\n=========\033[0m", msg, loc.file_name(), loc.line(), loc.column(), loc.function_name(), std::to_string(trace));
+	asm volatile("int3");
+	std::exit(-1);
+}
+
+int main(int argc, char** argv)
+{
+	panic("test");
 }
